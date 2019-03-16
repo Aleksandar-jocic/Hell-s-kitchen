45f01e41e4f836e3925f38a0dcfabd8f6ddbbade
tara.acimovic <tara.acimovic@gmail.com>
2019-03-16 14:51:57 +0100

    nesto

--- src/Recipe-card/recipe-card.js
+++ src/Recipe-card/recipe-card.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import './recipe-card.css';
 
-const Card = ({chef_src, user_name, pic_src}) => {
+
+
+const Card = ({id, chef_src, user_name, pic_src}) => {
+
     return (
-        <div class = 'recipe-card'>
+
+        <div id={`card-no${id}`} class = 'recipe-card col-6 col-md-4 p-1' >
+        <div class = 'recipe-container'>
             <img class='recipe' src={require(`../assets/Jela/${pic_src}.jpg`)} />
             <img class='chef' src={require(`../assets/kuvari/${chef_src}.png`)} />
-            <div class='recipe-name'>
-                <h2>{user_name}</h2>
+            <div class='recipe-name text-center mr-1 p-3'>
+                <h5>{user_name}</h5>
             </div>
         </div>
-);
+        </div>
 
+);
 }
 export default Card;
--- src/Recipes-card-list/recipes-card-list.js
+++ src/Recipes-card-list/recipes-card-list.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from '../Recipe-card/recipe-card';
 import { recipes } from '../BazaPrivremena/Baza-recepti';
+import './recipes-card-list.css'
 
 const CardList = ( ) => {
     const cardComponents = recipes.map((user,i) =>
-    {return <Card key ={i} id={recipes[i].id} chef_src={recipes[i].src_chef} user_name={recipes[i].name} pic_src = {recipes[i].src_name} />})
+    {return <Card  id={recipes[i].id} chef_src={recipes[i].src_chef} user_name={recipes[i].name} pic_src = {recipes[i].src_name}/>})
         return(
-            <div>
+            <div class='card-list'>
             {cardComponents}
             </div>
     )
